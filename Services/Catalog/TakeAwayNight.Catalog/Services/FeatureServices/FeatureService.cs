ba32cf5221ec78a03cb488ee2a15e6f0879acbfe
MuratYucedag <yucedagmurat23@gmail.com>
2024-07-11 22:32:22 +0300

    ...

---
+++ Services/Catalog/TakeAwayNight.Catalog/Services/FeatureServices/FeatureService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using TakeAwayNight.Catalog.Dtos.FeatureDtos;
+using TakeAwayNight.Catalog.Entities;
+using TakeAwayNight.Catalog.Settings;
+
+namespace TakeAwayNight.Catalog.Services.FeatureServices
+{
+    public class FeatureService : IFeatureService
+    {
+        private readonly IMongoCollection<Feature> _featureCollection;
+        private readonly IMapper _mapper;
+        public FeatureService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _featureCollection = database.GetCollection<Feature>(_databaseSettings.FeatureCollectionName);
+            _mapper = mapper;
+        }
+        public async Task CreateFeatureAsync(CreateFeatureDto createFeatureDto)
+        {
+            var value = _mapper.Map<Feature>(createFeatureDto);
+            await _featureCollection.InsertOneAsync(value);
+        }
+        public async Task DeleteFeatureAsync(string id)
+        {
+            await _featureCollection.DeleteOneAsync(x => x.FeatureId == id);
+        }
+        public async Task<List<ResultFeatureDto>> GetAllFeatureAsync()
+        {
+            var values = await _featureCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultFeatureDto>>(values);
+        }
+        public async Task<GetByIdFeatureDto> GetByIdFeatureAsync(string id)
+        {
+            var values = await _featureCollection.Find(x => x.FeatureId == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdFeatureDto>(values);
+        }
+        public async Task UpdateFeatureAsync(UpdateFeatureDto updateFeatureDto)
+        {
+            var values = _mapper.Map<Feature>(updateFeatureDto);
+            await _featureCollection.FindOneAndReplaceAsync(x => x.FeatureId == updateFeatureDto.FeatureId, values);
+        }
+    }
+}
