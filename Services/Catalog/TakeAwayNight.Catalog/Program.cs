c2750e560fde4ba4f30aa447a143d11dcff34eb9
MuratYucedag <yucedagmurat23@gmail.com>
2024-07-12 20:33:25 +0300

    ...

--- Services/Catalog/TakeAwayNight.Catalog/Program.cs
+++ Services/Catalog/TakeAwayNight.Catalog/Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using TakeAwayNight.Catalog.Services.CategoryServices;
+using TakeAwayNight.Catalog.Services.FeatureServices;
+using TakeAwayNight.Catalog.Services.ProductServices;
+using TakeAwayNight.Catalog.Services.SliderServices;
+using TakeAwayNight.Catalog.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<ISliderService, SliderService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IFeatureService, FeatureService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
