c364a9b8861d1ad8160770beafc3f4a64d7868d6
MuratYucedag <yucedagmurat23@gmail.com>
2024-07-19 21:39:41 +0300

    ...

---
+++ Services/Order/Core/TakeAwayNight.Application/Features/Mediator/Handlers/GetOrderingByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TakeAwayNight.Application.Features.Mediator.Queries;
+using TakeAwayNight.Application.Features.Mediator.Results;
+using TakeAwayNight.Application.Interfaces;
+using TakeAwayNight.Domain.Entities;
+
+namespace TakeAwayNight.Application.Features.Mediator.Handlers
+{
+    public class GetOrderingByIdQueryHandler : IRequestHandler<GetOrderingByIdQuery, GetOrderingByIdQueryResult>
+    {
+        private readonly IRepository<Ordering> _repository;
+        public GetOrderingByIdQueryHandler(IRepository<Ordering> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetOrderingByIdQueryResult> Handle(GetOrderingByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.Id);
+            return new GetOrderingByIdQueryResult
+            {
+                OrderDate = values.OrderDate,
+                OrderingId = values.OrderingId,
+                TotalPrice = values.TotalPrice,
+                UserId = values.UserId
+            };
+        }
+    }
+}
--- Services/Order/Core/TakeAwayNight.Application/Features/Mediator/Queries/GetOrderingByIdQuery.cs
+++ Services/Order/Core/TakeAwayNight.Application/Features/Mediator/Queries/GetOrderingByIdQuery.cs
@@ -5,10 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TakeAwayNight.Application.Features.CQRS.Results.OrderDetailResults;
+using TakeAwayNight.Application.Features.Mediator.Results;
 
 namespace TakeAwayNight.Application.Features.Mediator.Queries
 {
-    public class GetOrderingByIdQuery : IRequest<GetOrderDetailByIdQueryResult>
+    public class GetOrderingByIdQuery : IRequest<GetOrderingByIdQueryResult>
     {
         public int Id { get; set; }
         public GetOrderingByIdQuery(int id)
