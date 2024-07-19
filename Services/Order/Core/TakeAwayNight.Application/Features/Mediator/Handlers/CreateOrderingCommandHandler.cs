0283406600110160a19f06acb1cfc2918581c300
MuratYucedag <yucedagmurat23@gmail.com>
2024-07-19 21:50:21 +0300

    ...

---
+++ Services/Order/Core/TakeAwayNight.Application/Features/Mediator/Handlers/CreateOrderingCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TakeAwayNight.Application.Features.Mediator.Commands;
+using TakeAwayNight.Application.Interfaces;
+using TakeAwayNight.Domain.Entities;
+
+namespace TakeAwayNight.Application.Features.Mediator.Handlers
+{
+    public class CreateOrderingCommandHandler : IRequestHandler<CreateOrderingCommand>
+    {
+        private readonly IRepository<Ordering> _repository;
+        public CreateOrderingCommandHandler(IRepository<Ordering> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(CreateOrderingCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new Ordering
+            {
+                OrderDate = request.OrderDate,
+                TotalPrice = request.TotalPrice,
+                UserId = request.UserId
+            });
+        }
+    }
+}
---
+++ Services/Order/Core/TakeAwayNight.Application/Features/Mediator/Handlers/RemoveOrderingCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TakeAwayNight.Application.Features.Mediator.Commands;
+using TakeAwayNight.Application.Interfaces;
+using TakeAwayNight.Domain.Entities;
+
+namespace TakeAwayNight.Application.Features.Mediator.Handlers
+{
+    public class RemoveOrderingCommandHandler : IRequestHandler<RemoveOrderingCommand>
+    {
+        private readonly IRepository<Ordering> _repository;
+        public RemoveOrderingCommandHandler(IRepository<Ordering> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveOrderingCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.DeleteAsync(request.Id);
+        }
+    }
+}
