9782a95c1c87a3c725ceaf8ecda1ce7f7dfec7df
MuratYucedag <yucedagmurat23@gmail.com>
2024-07-19 20:18:03 +0300

    ...

---
+++ Services/Order/Core/TakeAwayNight.Application/Features/CQRS/Commands/OrderDetailCommands/CreateOrderDetailCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeAwayNight.Application.Features.CQRS.Commands.OrderDetailCommands
+{
+    public class CreateOrderDetailCommand
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Amount { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int OrderingId { get; set; }
+    }
+}
---
+++ Services/Order/Core/TakeAwayNight.Application/Features/CQRS/Commands/OrderDetailCommands/UpdateOrderDetailCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeAwayNight.Application.Features.CQRS.Commands.OrderDetailCommands
+{
+    public class UpdateOrderDetailCommand
+    {
+        public int OrderDetailId { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Amount { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int OrderingId { get; set; }
+    }
+}
